c5ca59b7c8559f1ef1b3c187c1dedd3225c09f6d
githubscientist <sathish.datascientist@gmail.com>
2024-08-18 11:15:57 +0530

    add new todo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,36 @@
-import { useLoaderData } from "react-router-dom";
+import { useState } from "react";
+import { useLoaderData, useNavigate } from "react-router-dom";
+import todoServices from "../services/todoServices";
 
 const Home = () => {
 
+  const [newTodo, setNewTodo] = useState('');
+  const [status, setStatus] = useState(false);
   const todos = useLoaderData();
+  const navigate = useNavigate();
+
+  const handleAddTodo = async (e) => {
+    e.preventDefault();
+    
+    // make a POST request to the server
+    todoServices.postTodo({
+      description: newTodo,
+      status: status
+    })
+      .then(response => {
+        alert('Todo added successfully');
+
+        // clear the form
+        setNewTodo('');
+        setStatus(false);
+
+        // reload the todos
+        navigate('/');
+      })
+      .catch(error => {
+        alert('Failed to add todo');
+      });
+  }
 
   return (
       <div>
@@ -16,6 +44,23 @@ const Home = () => {
           ))
         }
       </ul>
+
+      <form onSubmit={handleAddTodo}>
+        <input 
+          type="text"
+          placeholder="Add Todo..."
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <select
+          value={status}
+          onChange={(e) => setStatus(e.target.value)}
+        >
+          <option>False</option>
+          <option>True</option>
+        </select>
+        <button type="submit">Add Todo</button>
+      </form>
     </div>
   )
 }
--- src/services/todoServices.js
+++ src/services/todoServices.js
@@ -3,6 +3,9 @@ import instance from "./instance";
 const todoServices = {
     getTodos: async () => {
         return await instance.get('/todos');
+    },
+    postTodo: async (data) => {
+        return await instance.post('/todos', data);
     }
 }
 
